29ea9b6ae0bbfaaee2b0d189608b50f2593f892c
kazimayzid <kazimayzid208@gmail.com>
2025-10-01 02:42:36 +0600

    some design added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,9 @@
 import Particles from "../../ui/Particles";
 import Container from "../container/Container";
 import chatApp from "../../assets/chattingApp.png";
-import innovate from "../../assets/innovate.png"
-import petroil from "../../assets/petroil.png"
-import finsweet from "../../assets/finsweet.png"
+import innovate from "../../assets/innovate.png";
+import petroil from "../../assets/petroil.png";
+import finsweet from "../../assets/finsweet.png";
 import { Github, Link } from "lucide-react";
 export default function Projects({ dark }) {
   const projects = [
@@ -14,6 +14,7 @@ export default function Projects({ dark }) {
       livelink: "https://chatting-app-nine-mu.vercel.app/",
       img: chatApp,
       tags: ["React", "Tailwind", "Firebase", "Redux", "Motion"],
+      animation: "fade-left"
     },
     {
       title: "Innovate",
@@ -22,6 +23,7 @@ export default function Projects({ dark }) {
       livelink: "https://innovate-by-react.vercel.app/",
       img: innovate,
       tags: ["React", "Tailwind"],
+      animation: "fade-right"
     },
     {
       title: "Petroil",
@@ -30,6 +32,7 @@ export default function Projects({ dark }) {
       livelink: "https://petroil-tau.vercel.app/",
       img: petroil,
       tags: ["React", "Tailwind", "Redux"],
+      animation: "fade-left"
     },
     {
       title: "Finsweet",
@@ -38,11 +41,12 @@ export default function Projects({ dark }) {
       livelink: "https://finsweet-ashen-sigma.vercel.app/",
       img: finsweet,
       tags: ["React", "Tailwind"],
+      animation: "fade-right"
     },
   ];
   return (
     <>
-      <div className="relative w-full h-[1400px]">
+      <div className="relative w-full h-[1400px] mt-[150px]" data-aos="fade-up">
         <div className="absolute inset-0 z-0 bg-transparent">
           <Particles
             particleColors={["#524453", "#54578585"]}
@@ -62,56 +66,56 @@ export default function Projects({ dark }) {
               <h1 className="font-inter font-extrabold text-[45px] text-headingColor">
                 Projects
               </h1>
-              <p
+              <span
                 className={`${
                   dark ? "text-gradient-white" : "text-gradient"
                 } font-poppins font-semibold text-[14px] mb-[40px]`}
               >
                 Some of my Work
-              </p>
+              </span>
             </div>
-            {
-                projects.map((item, i) => (
-                    <div key={i} className="flex gap-x-[50px] mt-30 items-center justify-center">
-              <div className="w-[35%] h-[200px] rounded-[10px] overflow-hidden cursor-pointer">
-                <img
-                  className="w-full h-full object-cover rounded-xl object-center 
+            {projects.map((item, i) => (
+              <div
+                data-aos={item.animation}
+                key={i}
+                className="flex gap-x-[50px] mt-30 items-center justify-center"
+              >
+                <div className="w-[35%] h-[200px] rounded-[10px] overflow-hidden cursor-pointer">
+                  <img
+                    className="w-full h-full object-cover rounded-xl object-center 
                transform transition duration-300 hover:scale-105"
-                  src={item.img}
-                  alt="#"
-                />
-              </div>
-              <div className="w-[65%]">
-                <h1 className="font-inter font-bold text-navtext text-[30px]">
-                  {item.title}
-                </h1>
-                <p className="font-poppins font-normal text-detailsColor mt-5">
-                  {item.desc}
-                </p>
-                <div className="mt-5 flex gap-x-7 items-center ">
-                  <div className="flex items-center gap-x-3">
-                    <a href={item.livelink}>
-                      <Link className="text-navtext hover:text-headingColor duration-300 cursor-pointer" />
-                    </a>
-                    <a href={item.gitlink}>
-                      <Github className="text-navtext hover:text-headingColor duration-300 cursor-pointer" />
-                    </a>
-                  </div>
+                    src={item.img}
+                    alt="#"
+                  />
+                </div>
+                <div className="w-[65%]">
+                  <h1 className="font-inter font-bold text-navtext text-[30px]">
+                    {item.title}
+                  </h1>
+                  <p className="font-poppins font-normal text-detailsColor mt-5">
+                    {item.desc}
+                  </p>
+                  <div className="mt-5 flex gap-x-7 items-center ">
+                    <div className="flex items-center gap-x-3">
+                      <a href={item.livelink}>
+                        <Link className="text-navtext hover:text-headingColor duration-300 cursor-pointer" />
+                      </a>
+                      <a href={item.gitlink}>
+                        <Github className="text-navtext hover:text-headingColor duration-300 cursor-pointer" />
+                      </a>
+                    </div>
 
-                  <div className="flex gap-x-2">
-                    {
-                      item.tags.map((item)=> (
+                    <div className="flex gap-x-2">
+                      {item.tags.map((item) => (
                         <span className="font-poppins font-normal text-[14px] text-navtext border-[1px] border-borderColor px-4 py-0.5 hover:bg-bghover cursor-pointer hover:text-bg duration-300 rounded-full">
-                      {item}
-                    </span>
-                      ))
-                    }
+                          {item}
+                        </span>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-                ))
-            }
+            ))}
           </Container>
         </div>
       </div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,3 +1,24 @@
-export default function Services() {
-  return;
+import Container from "../container/Container";
+export default function Services({dark}) {
+  return (
+    <>
+      <Container>
+        <div className="mt-[220px]">
+          <div className="text-center">
+            <h1 className="font-inter font-extrabold text-[45px] text-headingColor">
+              What I do
+            </h1>
+            <span
+              className={`${
+                dark ? "text-gradient-white" : "text-gradient"
+              } font-poppins font-semibold text-[14px] mb-[40px]`}
+            >
+              My  Services
+            </span>
+          </div>
+          <div></div>
+        </div>
+      </Container>
+    </>
+  );
 }
